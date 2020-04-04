45a88bf2ff05497fc90676cfcfc6b1d7a6e00541
Gonzalez Martinez <orlandox.j.gonzalez.martinez@intel.com>
2020-04-03 21:43:13 -0600

    update js regardin video logo

--- js/script.js
+++ js/script.js
@@ -189,6 +189,12 @@ navBarItem.forEach(link => link.addEventListener('click', handleNavLinks));
 images.forEach(img => img.addEventListener('click', handleClickOnItem));
 exitButton.addEventListener('click', exitFunction);
 document.addEventListener('DOMContentLoaded', () => {
+    let vid = document.getElementById("video-logo");
+    console.log(vid, 'hhh');
+    vid.autoplay = true;
+    
     navBar.classList.add('menu-scrolled-down');
     interval = setInterval(startAnimationOnLoad, 3000);
+
+
 });
