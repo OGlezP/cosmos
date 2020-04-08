ac54e498ae52b8b4efab06c04c3367ef2cf39841
Gonzalez Martinez <orlandox.j.gonzalez.martinez@intel.com>
2020-04-07 21:57:03 -0500

    revert to mobile logo working2

--- js/script.js
+++ js/script.js
@@ -190,7 +190,6 @@ images.forEach(img => img.addEventListener('click', handleClickOnItem));
 exitButton.addEventListener('click', exitFunction);
 document.addEventListener('DOMContentLoaded', () => {
     let vid = document.getElementById("video-logo");
-    console.log(vid, 'hhh');
     vid.autoplay = true;
     
     navBar.classList.add('menu-scrolled-down');
